3bd403268f0af9796cee76dab04df2f916170cfd
kai10107 <kaiaustin@icloud.com>
2024-01-18 14:42:41 +1100

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 8;
+const num4 = 2;
+const mod = num3 / num4;
+
+const numbers = [5, 10, 20, 15, 3];
+const max = Math.max(...numbers);
+
+console.log(max);
